2b6669a6303e9506f13d9fbd7cd59c39b13f3cd0
RenattoHenrique <renato.trabalho17@gmail.com>
2025-02-17 19:34:51 -0300

    Atualizações

--- ProjetoLivrariaBancoDados/projeto.py
+++ ProjetoLivrariaBancoDados/projeto.py
@@ -2,13 +2,14 @@ import mysql.connector
 import os
 
 import Formatação_cores
-import Apresentação
-import Final
+import AnimacaoInicial
+import AnimacaoFinal
 
-conexao = mysql.connector.connect(host = "localhost", password = "", user = "root", database = "livraria", port = "3306")
+conexao = mysql.connector.connect(host = "localhost", password = "", user = "root", database = "livraria", port = "3307")
 cursor = conexao.cursor()
 
 #Rai
+
 def CadastrarLivro():
     os.system("cls")
     print("Insira os dados do novo livro:")
@@ -35,7 +36,7 @@ def CadastrarLivro():
         Menu()
     if not titulo or not genero or not sinopse or not autor or not publicacao or not valorCompra or not valorRevenda or len(publicacao) != 10:
         print("\033[31mDados inválidos. Verifique os dados e tente novamente.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
     try:
         cursor.execute(f"INSERT INTO livro (titulo, genero, sinopse, autor, publicacao, valor_compra, valor_revenda) VALUES ('{titulo}', '{genero}', '{sinopse}', '{autor}', '{publicacao}', '{valorCompra}', '{valorRevenda}')")
@@ -60,9 +61,9 @@ def ListarLivros():
         print("\033[31mNão há registros.\033[m")
         input()
         Menu()
-    print("="*50)
+    print("═"*50)
     print("Livros Cadastrados")
-    print("="*50)
+    print("═"*50)
     for linha in resultado:
         print(f"ID: {linha[0]}")
         print(f"Título: {linha[1]}")
@@ -72,7 +73,7 @@ def ListarLivros():
         print(f"Sinopse: {linha[5]}\n")
         print("-"*50)
     print("\033[32mListagem Concluída com sucesso.\033[m")
-    input()
+    input("\nDigite ENTER para retornar ao Menu...")
     Menu()
 
 def ListarEstoque():
@@ -83,9 +84,9 @@ def ListarEstoque():
         print("\033[31mNão há livros registrados para listar um estoque.\033[m")
         input()
         Menu()
-    print("="*50)
+    print("═"*50)
     print("ESTOQUE")
-    print("="*50)
+    print("═"*50)
     for linha in resultado:
         print(f"livro ID: {linha[0]}")
         print(f"Título: {linha[1]}")
@@ -94,11 +95,12 @@ def ListarEstoque():
         print(f"Quantidade disponível: {linha[4]}\n")
         print("-"*50)
     print("\033[32mListagem concluída com sucesso.\033[m")
-    input()
+    input("\nDigite ENTER para retornar ao Menu...")
     Menu()
 
 
 #Renato
+
 def ComprarLivros():
     os.system("cls")
     id_livro = input("Insira o ID do livro a ser comprado: ")
@@ -110,7 +112,7 @@ def ComprarLivros():
     resultado = cursor.fetchall()
     if len(resultado) == 0:
         print(f"\033[31mNão há livro registrado com o id {id_livro}\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
     for linha in resultado:
         titulo = linha[1]
@@ -120,12 +122,12 @@ def ComprarLivros():
         quantidade = int(input())
     except ValueError:
         print("\033[31mDado inválido.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
     data_compra = input("Registre a data da compra. (AAAA/MM/DD): ") 
     if len(data_compra) != 10:
         print("\033[31m data precisa ter 10 caracteres.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu() 
     custo = valor_compra * quantidade
     try:
@@ -139,10 +141,10 @@ def ComprarLivros():
         cursor.execute(f"UPDATE estoque SET quantidade = {atualizarQuantidade} WHERE id_livro = {id_livro}")
         conexao.commit()
         print("\033[32mCompra registrada com sucesso.\033[m") 
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
     except:
         print("\033[31mNão foi possível registrar a compra. verifique os dados e tente novamente.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
     Menu()
 
 def ListarCompras():
@@ -151,11 +153,11 @@ def ListarCompras():
     resultado = cursor.fetchall()
     if len(resultado) == 0:
         print("\033[31mNão há registro.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
-    print("="*50)
+    print("═"*50)
     print("REGISTRO DE COMPRAS")
-    print("="*50)
+    print("═"*50)
     for linha in resultado:
         id = linha[0]
         id_livro = linha[1]
@@ -171,7 +173,7 @@ def ListarCompras():
         print(f"Custo total: {custo}R$\n")
         print("-"*50)
     print("\033[32mListagem concluída com sucesso.\033[m")
-    input()
+    input("\nDigite ENTER para retornar ao Menu...")
     Menu()
 
 def VenderLivros():
@@ -180,7 +182,7 @@ def VenderLivros():
     disponibilidade = cursor.fetchall()
     if len(disponibilidade) == 0:
         print("\033[31mNão há livros cadastrados para a venda.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
     id_livro = input("Insira o ID do livro a ser vendido: ")
     if id_livro.lower() == "sair":
@@ -191,7 +193,6 @@ def VenderLivros():
     resultado = cursor.fetchall()
     if len(resultado) == 0:
         print(f"\033[31mNão há livro registrado com o id {id_livro}\033[m")
-        input()
         VenderLivros()
     for linha in resultado:
         id = linha[0]
@@ -204,7 +205,7 @@ def VenderLivros():
         quantidadeEstocada = linha[0]
     if quantidadeEstocada == 0:
         print(f"\033[31mNão há cópias do livro {titulo} disponíveis a serem vendidas.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
     id_cliente = input(f"Insira o ID do cliente que irá comprar o livro {titulo}: ")
     if id_cliente.lower() == "sair":
@@ -242,7 +243,7 @@ def VenderLivros():
         input()
     except:
         print("\033[32mNão foi possível registrar a venda. verifique os dados e tente novamente.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
     Menu()
 
 def ListarVendas():
@@ -251,11 +252,11 @@ def ListarVendas():
     resultado = cursor.fetchall()
     if len(resultado) == 0:
         print("\033[31mNão há registro.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
-    print("="*50)
+    print("═"*50)
     print("LISTA DE VENDAS")
-    print("="*50)
+    print("═"*50)
     for linha in resultado:
         v_id = linha[0]
         l_id = linha[1]
@@ -273,7 +274,7 @@ def ListarVendas():
         print(f"Lucro: {v_lucro}R$\n")
         print("-"*50)
     print("\033[32mListagem concluída com sucesso.\033[m")
-    input()
+    input("\nDigite ENTER para retornar ao Menu...")
     Menu()
 
 
@@ -310,11 +311,11 @@ def RemoverLivro():
             cursor.execute(f"DELETE FROM livro WHERE id = {id}")
             conexao.commit()
             print("\033[32mRegistro deletado com sucesso.\033[m")
-            input()
+            input("\nDigite ENTER para retornar ao Menu...")
             Menu()
         except:
             print("\031[32mNão foi possível deletar o cadastro por um erro sinistro.\031[m")
-            input("Tecle Enter para retornar ao menu: ")
+            input("\nDigite ENTER para retornar ao Menu...")
             Menu()
     else:
         Menu()
@@ -363,7 +364,7 @@ def AtualizarLivro():
     if not novoTitulo or not novoGenero or not novoAutor or not novaPublicacao or not novaSinopse or not novoValorCompra or not novoValorVenda or len(novaPublicacao) != 10 or novoValorCompra < 0 or novoValorVenda < 0:
         os.system("cls")
         print("\033[31mDados inválidos. Verifique os dados e tente novamente.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
     print("\033[33mCONFIRMAÇÃO:\033[m Deseja realmente Atualizar o registro atual ? S/N")
     confirmacao = input()
@@ -377,7 +378,7 @@ def AtualizarLivro():
     else:
         os.system("cls")
         print("\033[32mAtualização cancelada.\033[m")
-    input()
+    input("\nDigite ENTER para retornar ao Menu...")
     Menu()
 
 def CadastrarCliente():
@@ -402,7 +403,7 @@ def CadastrarCliente():
         print("\033[32mCadastro registrado com sucesso.\033[m")
     except:
         print("\033[31mNão foi possível registrar o cadastro. Verifique os dados e tente novamente.\033[m")
-    input()
+    input("\nDigite ENTER para retornar ao Menu...")
     Menu()
 
 def ListarClientes():
@@ -411,7 +412,7 @@ def ListarClientes():
     resultado = cursor.fetchall()
     if len(resultado) == 0:
         print("\033[31mNão há registros.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
     print("="*50)
     print("Clientes cadastrados")
@@ -423,7 +424,7 @@ def ListarClientes():
         print(f"E_mail: {linha[3]}\n")
         print("-"*50)
     print("\033[32mListagem Concluída com sucesso.\033[m")
-    input()
+    input("\nDigite ENTER para retornar ao Menu...")
     Menu()
 
 
@@ -459,7 +460,7 @@ def AtualizarCliente():
     newEmail = input("Novo E-mail: ")
     if not newNome or not newTelefone or not newEmail or len(newTelefone) != 11:
         print("\033[31mDados inválidos. O registro não foi atualizado.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
     print("\033[33mCONFIRMAÇÃO:\033[m Deseja realmente Atualizar o registro atual ? S/N")
     confirmacao = input()
@@ -473,7 +474,7 @@ def AtualizarCliente():
     else:
         os.system("cls")
         print("\033[32mAtualização cancelada.\033[m")
-    input()
+    input("\nDigite ENTER para retornar ao Menu...")
     Menu()
 
 def AdicionarAvaliacoes():
@@ -523,7 +524,7 @@ def AdicionarAvaliacoes():
         print("\033[32mComentário adicionado com sucesso.\033[m")
     except:
         print("\033[31mNão foi possível adicionar o comentário em questão.\033[m")
-    input()
+    input("\nDigite ENTER para retornar ao Menu...")
     Menu()
     
 def VerificarAvaliacoes():
@@ -545,7 +546,7 @@ def VerificarAvaliacoes():
         print(f"Comentário: {linha[4]}\n")
         print("-"*50)
     print("\033[32mConsulta concluída com sucesso.\033[m")
-    input()
+    input("\nDigite ENTER para retornar ao Menu...")
     Menu()
     
 
@@ -587,7 +588,7 @@ def RemoverCliente():
             Menu()
         except:
             print("\031[32mNão foi possível deletar o cadastro por um erro sinistro.\031[m")
-            input("Tecle Enter para retornar ao menu: ")
+            input("\nDigite ENTER para retornar ao Menu...")
             Menu()
     else:
         Menu()
@@ -622,7 +623,7 @@ def deletarAvaliacao():
             input()
         except:
             print("\033[31mNão foi possível deletar o registro.\033[m")
-            input()
+            input("\nDigite ENTER para retornar ao Menu...")
         Menu()
 
 def EncerrarConexao():
@@ -634,10 +635,8 @@ def EncerrarConexao():
         os._exit(0)
     except:
         print("\033[31mNão foi possível encerrar a conexão. Tente novamente.\033[m")
-        input()
+        input("\nDigite ENTER para retornar ao Menu...")
         Menu()
-
-
 
 def Menu():  
     os.system("cls")
@@ -693,56 +692,10 @@ def Menu():
             opcao = input()  
             if opcao.lower() == "s":  
                 os.system("cls")
-                Final.run_asciimatics_demo()  
+                AnimacaoFinal.run_asciimatics_demo()  
             else:  
                 continue  
         else:  
-            continue       
-Apresentação.animacao_intro()  
+            continue  
+
 Menu()
-
-
-
-
-
-
-
-
-# def deletarCompra(): 
-#     os.system("cls")
-#     id = input("Insira o ID da compra a ser deletada: ")
-#     if not id:
-#         deletarCompra()
-#     cursor.execute(f"SELECT * FROM compra WHERE id = {id}")
-#     resultado = cursor.fetchall()
-#     if len(resultado) == 0:
-#         print(f"\033[31mNão há registro de compra registrada com o ID {id}\033[m")
-#         input()
-#         deletarCompra()
-#     print("\033[32mRegistro de compra encontrado !\033[m")
-#     print("-"*50)
-#     for linha in resultado:
-#         id = linha[0]
-#         id_livro = linha[1]
-#         titulo = linha[2]
-#         quantidade = linha[3]
-#         data_compra = linha[4]
-#         custo = linha[5]
-#         print(f"ID da compra: {id}")
-#         print(f"ID do livro: {id_livro}")
-#         print(f"Livro: {titulo}")
-#         print(f"Quantidade comprada: {quantidade}")
-#         print(f"Data da compra: {data_compra}")
-#         print(f"Custo total: {custo}R$\n")
-#         print("-"*50)
-#     deletar = input("Você realmente deseja apagar esse registro ? (S/N): ")
-#     if deletar.lower() == "s":
-#         try:
-#             cursor.execute(f"DELETE FROM avaliacao WHERE id = {id}")
-#             conexao.commit()
-#             print("\033[32mRegistro deletado com sucesso.\033[m")
-#             input()
-#         except:
-#             print("\033[31mNão foi possível deletar o registro.\033[m")
-#             input()
-#         Menu()
